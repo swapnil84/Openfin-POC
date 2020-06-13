aaf63b8daf0a1a6b0473af25c08512e52ca8cc43
swapnil.tivlekar <swapnil.tivlekar@gmail.com>
2020-06-12 20:51:49 -0700

    Openfin functional changes (#1)
    * adding gitignore file
    
    * openfin poc new changes

--- src/components/stock-details/stock-details.js
+++ src/components/stock-details/stock-details.js
@@ -1,26 +1,14 @@
 var stockDetails;
 
-// const httpGetAsync = (theUrl, callback) => {
-//     var xmlHttp = new XMLHttpRequest();
-//     xmlHttp.onreadystatechange = function() { 
-//         if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-//             callback(xmlHttp.responseText);
-//     }
-//     xmlHttp.open("GET", theUrl, true); // true for asynchronous 
-//     xmlHttp.send(null);
-// }
-
 const getData = function (url) {
     axios.get(url).then(function (res) {
-        console.log("url: " + url + "res.data" + res.data);
-        const listItem = `<ul>
-            <li>Previous Close - ${res.data.pc}</li>
-            <li>Open - ${res.data.o}</li>
-            <li>Day's Range - ${res.data.l} - ${res.data.h}</li>
-        </ul>`
-        _stockDetailsWrapper.innerHTML = listItem
-        updateDate(data);
-        // gridOptions.api.setColumnDefs(this.getColumns(response.data));
+        // const listItem = `<ul>
+        //     <li>Previous Close - ${res.data.pc}</li>
+        //     <li>Open - ${res.data.o}</li>
+        //     <li>Day's Range - ${res.data.l} - ${res.data.h}</li>
+        // </ul>`
+        // _stockDetailsWrapper.innerHTML = listItem
+        updateDate(res.data);
     })
     .catch(function (error) {
         console.log(error);
@@ -32,7 +20,6 @@ const getData = function (url) {
 
 const initInterApp = () => {
   _stockNameWrapper = document.querySelector("#stock-name-wrapper")
-  _stockDetailsWrapper = document.querySelector("#stock-details-wrapper")
   
   fin.desktop.InterApplicationBus.addSubscribeListener(function (uuid, topic) {
       console.log("The application " + uuid + " has subscribed to " + topic);
@@ -43,13 +30,6 @@ const initInterApp = () => {
       function (message, uuid) {
         const url = 'https://finnhub.io/api/v1/quote?symbol='+message.symbol+'&token=brgslqnrh5r9t6gjebng';
         getData(url);
-        // httpGetAsync(url, function(response){
-        //     console.log(response);
-        //     const res = JSON.parse(response);
-        //     const listItem = '<ul><li>Open - '+res.o+'</li></ul>'
-        //     _stockDetailsWrapper.innerHTML = listItem
-        //     // document.getElementById('stock-details-wrapper').appendChild(listItem);
-        // })
         _stockNameWrapper.innerHTML = message.description+' ('+message.symbol+')';
       });
 };
@@ -63,6 +43,11 @@ const initWithOpenFin = () => {
 }
 
 function init(){
+    _currentTimeWrapper = document.querySelector("#current-time");
+    var today = new Date();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    _currentTimeWrapper.innerHTML = time;
+
     try{
         fin.desktop.main(function(){
             initWithOpenFin();
@@ -84,16 +69,16 @@ var data = {
     earningDate: "314.75 - 318.52",
 }
 
-function updateDate(data){
-    document.getElementById("prevClose").innerText = data.prevClose;
-    document.getElementById("open").innerText = data.open;
-    document.getElementById("dayRange").innerText = data.dayRange;
-    document.getElementById("weekRange").innerText = data.weekRange;
-    document.getElementById("marketCap").innerText = data.marketCap;
-    document.getElementById("beta5YMonthly").innerText = data.beta5YMonthly;
-    document.getElementById("peRatio").innerText = data.peRatio;
-    document.getElementById("eps").innerText = data.eps;
-    document.getElementById("earningDate").innerText = data.earningDate;
+function updateDate(res){
+    document.getElementById("prevClose").innerText = `${res.pc}`;
+    document.getElementById("open").innerText = `${res.o}`;
+    document.getElementById("dayRange").innerText = `${res.l} - ${res.h}`;
+    // document.getElementById("weekRange").innerText = data.weekRange;
+    // document.getElementById("marketCap").innerText = data.marketCap;
+    // document.getElementById("beta5YMonthly").innerText = data.beta5YMonthly;
+    // document.getElementById("peRatio").innerText = data.peRatio;
+    // document.getElementById("eps").innerText = data.eps;
+    // document.getElementById("earningDate").innerText = data.earningDate;
 }
 
 (function () {
